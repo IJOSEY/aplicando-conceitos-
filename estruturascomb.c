39d210fb8d6c5c64b0a49c6f04a918b38c3cdfdd
IJOSEY <ijoseybs@GMAIL.COM>
2024-11-29 14:09:01 +0000

    código imprime um menu p/ calculo de média

--- estruturascomb.c
+++ estruturascomb.c
@@ -1,1 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main (){
+    int opcao;
+    float nota1, nota2, nota3, media;
 
+    printf("*** Menu do aluno ***\n\n");
+    printf("1. Calcular Média\n");
+    printf("2. Determinar Status\n");
+    printf("3. Sair\n");
+    printf("Escolha uma opção: ");
+    scanf("%d", &opcao);
+    
+    switch (opcao)
+    {
+    case 1:
+        printf("Digite a primeira nota: ");
+        scanf("%f",&nota1);
+        printf("Digite a segunda nota: ");
+        scanf("%f",&nota2);
+        printf("Digite a terceira nota: ");
+        scanf("%f",&nota3);
+        media = (nota1 + nota2 + nota3)/3;
+        printf("A média é de: %.2f\n",media);
+        break;
+    case 2:
+        /* code */
+        break;
+    case 3:
+        printf("Finalizando sessão!!");
+        break;
+    
+    default:
+        break;
+    }
+}
